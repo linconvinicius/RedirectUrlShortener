2d30aab9527fec08aff95c4d241e0fff8f17eed3
linconvinicius <lvini_@live.com>
2024-11-21 16:42:28 -0300

    update scripts

--- src/main/java/com/rocketseat/redirectUrlShortener/Main.java
+++ src/main/java/com/rocketseat/redirectUrlShortener/Main.java
@@ -1,17 +1,68 @@
 package com.rocketseat.redirectUrlShortener;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+
+    private final S3Client s3Client = S3Client.builder().build();
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+        String pathParameters = (String) input.get("rawPath");
+        String shortUrlCode = pathParameters.replace("/", "");
+
+        if (shortUrlCode == null || shortUrlCode.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input: 'shortUrlCode' is required.");
         }
+
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket("lambda-shortener-url")
+                .key(shortUrlCode + ".json")
+                .build();
+
+        InputStream s3ObjectStream;
+
+        try {
+            s3ObjectStream = s3Client.getObject(getObjectRequest);
+        } catch (Exception exception) {
+            throw new RuntimeException("Error fetching data from S3: " + exception.getMessage(), exception);
+        }
+
+        UrlData urlData;
+        try {
+            urlData = objectMapper.readValue(s3ObjectStream, UrlData.class);
+        } catch (Exception exception) {
+            throw new RuntimeException("Error deserializing URL data: " + exception.getMessage(), exception);
+        }
+
+        long currentTimeInSeconds = System.currentTimeMillis() / 1000;
+
+        Map<String, Object> response = new HashMap<>();
+
+        // cenario onde a URL expirou
+        if(urlData.getExpirationTime() < currentTimeInSeconds) {
+            response.put("statusCode", 410);
+            response.put("body", "This URL has expired.");
+
+            return response;
+        }
+
+        // cenario onde a URL ainda é válida
+        response.put("statusCode", 302);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Location", urlData.getOriginalUrl());
+        response.put("headers", headers);
+
+        return response;
     }
 }
